59261381e4dc367326a33cbfadf37ee6b42e3751
Tadiwanashe Dzvoti <tadiwadzvoti@hotmail.com>
2024-02-29 16:35:26 +0000

    Add working get request for all flights.

---
+++ src/main/java/com/example/airline_api/components/DataLoader.java
@@ -0,0 +1,32 @@
+package com.example.airline_api.components;
+
+import com.example.airline_api.models.Flight;
+import com.example.airline_api.repositories.FlightRepository;
+import com.example.airline_api.repositories.PassengerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements ApplicationRunner {
+
+    @Autowired
+    FlightRepository flightRepository;
+
+    @Autowired
+    PassengerRepository passengerRepository;
+
+    public DataLoader(){
+
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        Flight flight1 = new Flight("AWDA",100, "ads", "asda");
+        flightRepository.save(flight1);
+
+        Flight flight2 = new Flight("w",1, "d", "f");
+        flightRepository.save(flight2);
+    }
+}

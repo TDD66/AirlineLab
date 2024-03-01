5fc4f0e8a0c1dfd419bf131bcc71f489722f0c07
Tadiwanashe Dzvoti <tadiwadzvoti@hotmail.com>
2024-03-01 09:41:41 +0000

    Finish extensions.

--- src/main/java/com/example/airline_api/components/DataLoader.java
+++ src/main/java/com/example/airline_api/components/DataLoader.java
@@ -9,6 +9,10 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.Month;
+
 @Component
 public class DataLoader implements ApplicationRunner {
 
@@ -24,10 +28,10 @@ public class DataLoader implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        Flight flight1 = new Flight("AWDA",100, "ads", "asda");
+        Flight flight1 = new Flight("AWDA", 100, LocalDate.of(2020, Month.AUGUST, 12), LocalTime.of(6, 30));
         flightRepository.save(flight1);
 
-        Flight flight2 = new Flight("w",1, "d", "f");
+        Flight flight2 = new Flight("dsa", 50, LocalDate.of(2020, Month.SEPTEMBER, 30), LocalTime.of(10, 30));
         flightRepository.save(flight2);
 
         Passenger passenger1 = new Passenger("ASDSA", "ASDAA");
